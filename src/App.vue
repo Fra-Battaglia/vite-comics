a78caaeedf3b693330157224db15f26bbeed9641
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-01-11 21:23:36 +0100

    AppContent added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script>
 	import AppHeader from './components/AppHeader.vue';
+	import AppContent from './components/AppContent.vue';
 	export default {
 		components: {
 			AppHeader,
+			AppContent,
 		}
 	}
 </script>
 
 <template>
 		<AppHeader />
+		<AppContent />
 </template>
 
 <style lang="scss">
+
+	@use './components/styles/partials/mixins' as *;
+	@use './components/styles/partials/variables' as *;
+
 	@import url('./components/styles/generals.scss');
 </style>
---
+++ src/components/AppContent.vue
@@ -0,0 +1,31 @@
+<script>
+	export default {
+		data() {
+			return {
+				Message: '-->> Content Goes Here <<--'
+			}
+		}
+	}
+</script>
+
+<template>
+	<main>
+		<div class="container">
+			<h1>{{ Message }}</h1>
+		</div>
+	</main>
+
+</template>
+
+<style lang="scss" scoped>
+
+	@use './styles/partials/mixins' as *;
+	@use './styles/partials/variables' as *;
+
+	main {
+		height: 100px;
+		background-color: $dark-color;
+		color: $light-color;
+		@include flex-align-items-center;
+	}
+</style>
